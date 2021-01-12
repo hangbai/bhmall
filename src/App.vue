888a71a2abd4c55cdcbdf29ef66526f9cf44760c
Hang Bai <baihang@gmail.com>
2021-01-12 10:28:02 +0100

    home

--- src/App.vue
+++ src/App.vue
@@ -11,18 +11,7 @@ import BottomNavigation from "@/components/BottomNavigation";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BottomNavigation
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
